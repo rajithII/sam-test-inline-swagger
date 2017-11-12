81c67dfc5e59816fa7f12db60b1fd3d0fbbb7a06
rajithII <30283449+rajithII@users.noreply.github.com>
2017-11-12 13:54:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 exports.handler = function(event, context, callback) {
     callback(null, {
         "statusCode": 200,
-        "body": "hello world!" 
+        "body": "hello world lambda!" 
     });
 }
 
