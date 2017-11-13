6c31f81e50c21360ab78f2e108e06bb57d7dedc9
rajithII <30283449+rajithII@users.noreply.github.com>
2017-11-13 14:59:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Canada is: ' + currentTime.toString(),
+        body: 'The time in Mexico is: ' + currentTime.toString(),
     });
 };
 
