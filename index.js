aaa174984b0dc18468bea1619528be08ccb1ea6e
rajithII <30283449+rajithII@users.noreply.github.com>
2017-11-13 18:53:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-var time = require('time');
-exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
+exports.handler = function(event, context, callback) {
     callback(null, {
-        statusCode: '200',
-        body: 'The Time In Texas is: ' + currentTime.toString(),
+        "statusCode": 200,
+        "body": "hello world" 
     });
-};
-
+}
 
